cfe6f20038b8726a7795e83463e109f0c0398f8e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-24 19:43:39 +0100

    styled hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,19 +1,22 @@
-//importing next utils
-import Image from 'next/image';
 //hero
 const Hero = () => {
   return (
     <section className="hero__section">
-      <div className="text__content">
-        <h1>Hi, I’m Achraf, Front End Web Developer and Web Designer</h1>
-        <p>
-          I design and build beautiful websites for businesses around the globe.
-          If you need a modern and powerful website, send me an email. If we are
-          a good fit, I will give you a time and cost estimate.
-        </p>
-        <button className="primary__btn">see my work</button>
+      <div className="container">
+        <div className="text__content">
+          <h1>Hi, I’m Achraf, Front End Web Developer and Web Designer</h1>
+          <p>
+            I design and build beautiful websites for businesses around the
+            globe. If you need a modern and powerful website, send me an email.
+            If we are a good fit, I will give you a time and cost estimate.
+          </p>
+          <div className="primary__btn__container">
+            <button className="primary__btn">see my work</button>
+            <div className="ghost__button"></div>
+          </div>
+        </div>
+        <div className="illustration__container"></div>
       </div>
-      <div className="illustration__container"></div>
     </section>
   );
 };
