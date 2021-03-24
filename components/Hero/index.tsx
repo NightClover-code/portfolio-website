5a0052149ae1ca8160a528e1481bb7cbdd060e11
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-24 19:20:26 +0100

    added hero section

---
+++ components/Hero/index.tsx
@@ -0,0 +1,21 @@
+//importing next utils
+import Image from 'next/image';
+//hero
+const Hero = () => {
+  return (
+    <section className="hero__section">
+      <div className="text__content">
+        <h1>Hi, I’m Achraf, Front End Web Developer and Web Designer</h1>
+        <p>
+          I design and build beautiful websites for businesses around the globe.
+          If you need a modern and powerful website, send me an email. If we are
+          a good fit, I will give you a time and cost estimate.
+        </p>
+        <button className="primary__btn">see my work</button>
+      </div>
+      <div className="illustration__container"></div>
+    </section>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,10 @@
-const Homepage = () => <main className="app__container">hey hey</main>;
+//importing components
+import Hero from '../components/Hero';
+//homepage
+const Homepage = () => (
+  <main className="app__container">
+    <Hero />
+  </main>
+);
+
 export default Homepage;
