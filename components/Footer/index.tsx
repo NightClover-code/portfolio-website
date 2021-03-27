333ba2b3d4e2396e90d3e0717038b2eb24f50c97
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-27 15:17:33 +0100

    finished first prototype of the website

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,28 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer__head">
+          <h1>
+            Want to
+            <span className="line__container">
+              <span> work?</span>
+              <div className="line"></div>
+            </span>{' '}
+          </h1>
+          <p>
+            If you need a modern and powerful website for your business, startup
+            or yourself, I am available for work. You can email me directly at
+            <span className="line__container">
+              <span> achrafdev23@gmail.com</span>
+              <div className="line"></div>
+            </span>{' '}
+          </p>
+        </div>
+        <div className="copyright">&copy; All rights reserved - achrafdev</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
