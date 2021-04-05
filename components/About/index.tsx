f44ea5e3d655297111513d10e9906647a400a0f8
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-05 21:36:40 +0200

    added an about component

---
+++ components/About/index.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div>about</div>;
+};
+
+export default About;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,7 @@
 //importing utils
 import Head from 'next/head';
+//importing components
+import About from '../components/About';
 //about page
 const AboutPage = () => {
   return (
@@ -12,6 +14,9 @@ const AboutPage = () => {
         <meta name="author" content="Achraf Dev" />
         <meta name="keywords" content="" />
       </Head>
+      <div className="app__container">
+        <About />
+      </div>
     </>
   );
 };
