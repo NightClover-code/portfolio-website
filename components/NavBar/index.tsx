b8caecb972cc5522925e7332a83f50d3dd07e658
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-06 13:55:35 +0200

    removing about page

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -63,9 +63,6 @@ const NavBar = () => {
           <li onClick={() => onNavLinkClickHandler('.footer', 1200)}>
             Get In Touch
           </li>
-          <Link href="/about">
-            <li>About Me</li>
-          </Link>
           <li
             className="portfolio__list__item"
             onClick={() =>
