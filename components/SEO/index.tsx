5b08db0bdf62c8c6c3e9c26b0c71836246406340
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-11 15:56:58 +0200

    returning to using favicon.ico

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -11,11 +11,7 @@ const SEO: React.FC<SE0Props> = ({ title, description, keywords }) => {
   return (
     <Head>
       <title>{title}</title>
-      <link
-        rel="shortcut icon"
-        type="images/x-generic"
-        href="/images/favicon.png"
-      />
+      <link rel="shortcut icon" type="images/ico" href="/images/favicon.ico" />
       <meta name="description" content={description} />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       {keywords && <meta name="keywords" content={keywords} />}
