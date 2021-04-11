e7855ea608c4eb84a1f59c5d61b1d81a370e065a
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-11 15:40:29 +0200

    changed favicon size

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -11,7 +11,11 @@ const SEO: React.FC<SE0Props> = ({ title, description, keywords }) => {
   return (
     <Head>
       <title>{title}</title>
-      <link rel="shortcut icon" type="images/ico" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        type="images/ico"
+        href="/images/favicon.ico"
+      />
       <meta name="description" content={description} />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       {keywords && <meta name="keywords" content={keywords} />}
