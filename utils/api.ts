0ff105ca0da52c7e56881bf854d5f7e44100031e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-19 14:59:27 +0200

    cleaning up some images

--- utils/api.ts
+++ utils/api.ts
@@ -2,5 +2,7 @@
 import axios from 'axios';
 //api base url
 export const portfolioAPI = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_STRAPI_API_URL ||
+    'https://achraf-portfolio-strapi-api.herokuapp.com/',
 });
