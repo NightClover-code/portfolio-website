81f784eb6ae20baeb074f4f5c93ae084ef65e26e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-05-31 16:57:26 +0100

    adding more images to cloudinary and cleanup

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -11,7 +11,11 @@ const SEO: React.FC<SE0Props> = ({ title, description, keywords }) => {
   return (
     <Head>
       <title>{title}</title>
-      <link rel="shortcut icon" type="images/ico" href="/images/favicon.ico" />
+      <link
+        rel="shortcut icon"
+        type="images/ico"
+        href="https://res.cloudinary.com/achraf-dev/image/upload/v1622476542/favicon_agfwor.png"
+      />
       <meta name="description" content={description} />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       {keywords && <meta name="keywords" content={keywords} />}
